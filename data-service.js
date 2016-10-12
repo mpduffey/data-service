090205dd43b2f87f2316c79e8a55c1526fede152
Mike Duffey <mpduffey@gmail.com>
2016-10-12 05:58:30 +0000

    y

--- data-service.js
+++ data-service.js
@@ -10,7 +10,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var http_1 = require('@angular/http');
-require('rxjs/Rx');
+require('rxjs/add/operator/map');
 var DataService = (function () {
     function DataService(http) {
         var _this = this;
